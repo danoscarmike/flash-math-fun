1de3efac949967c7d8366fdfee6b6f961cd7cbc4
Dan O'Meara <omeara.dan@gmail.com>
2025-09-10 21:28:00 -0700

    renames game service to questions

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,3 @@
-from .game import generate_question_pool, get_max_valid_questions
+from .questions import generate_question_pool, get_max_valid_questions
 
 __all__ = ["generate_question_pool", "get_max_valid_questions"]
